3a12076eba0616a00c2cfb96e52a1ff9302fec3c
itsanerin <31420768+itsanerin@users.noreply.github.com>
2019-12-09 16:34:05 -0500

    ban rape jokes

--- server.rb
+++ server.rb
@@ -4,6 +4,10 @@ set :port, 3000
 
 get '/' do
   @joke = ChuckNorris.random
+ while @joke.include?("rape")
+  puts "Rape jokes are not okay!"
+   @joke = ChuckNorris.random
+ end
 
   erb :index
 end
